b2985ae2944282ab58d0f5db05ad99b1c9e70259
cng12 <granocn@clarkson.edu>
2025-11-19 15:00:22 -0500

    Database management

--- auth.py
+++ auth.py
@@ -25,6 +25,8 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+
+received_email = False
 token = str(random.randint(100000, 999999))
 
 def SendEmail(email,body,sub):
@@ -47,13 +49,16 @@ def SendEmail(email,body,sub):
 def is_valid_email(email):
     return "@" in email and "." in email
 
-@auth.route("/get_token")
-def sendToAndroid():
-    listen()
-    return jsonify({"token": token})
+# @auth.route("/get_token")
+# def sendToAndroid():
+#     listen()
+#     return jsonify({"token": token})
 
 
+
+@auth.route("/listen")
 def listen():
+    global received_email
     HOST = "0.0.0.0"
     PORT = 7000
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -63,70 +68,113 @@ def listen():
     conn, addr = server.accept()
     print(f"Connected by: {addr}")
 
+
     data = conn.recv(4096)
     text = data.decode('utf-8').strip()
 
-    if (is_valid_email(text)):
-        emailMess = (
-                        "To complete your registration for your EE368Project account please use the following verification code.\n\n"
-                        "Verification Code: ") + token + (
-                        "\n\n\nIf you are not trying to register this email address, please ignore this.")
-        SendEmail(text, emailMess, "Email Verification - [" + token + "]")
+    while True:
+        if (is_valid_email(text) and not received_email):
+            emailMess = (
+                            "To complete your registration for your EE368Project account please use the following verification code.\n\n"
+                            "Verification Code: ") + token + (
+                            "\n\n\nIf you are not trying to register this email address, please ignore this.")
+            SendEmail(text, emailMess, "Email Verification - [" + token + "]")
+            received_email = True
+            print(token)
+            return jsonify({"token": token})
 
 
-    else:
 
-        while True:
-            data = conn.recv(4096)
-            if not data:
-                break
+        else:
+            print("Server listening...")
+            while True:
+                data = conn.recv(4096)
+                if not data:
+                    break
 
-            text = data.decode('utf-8').strip()
-            if not text:
-                continue
+                text = data.decode('utf-8').strip()
+                if not text:
+                    continue
 
-            try:
-                features = json.loads(text)
-                print("Received Features:", features)
+                try:
+                    features = json.loads(text)
+                    print("Received Features:", features)
 
-                # ---------- Insert into MySQL ----------
-                sql = """
-                INSERT INTO swipefeatures
-                (userID, strokeDuration, midStrokeArea, midStrokePress, dirEndToEnd, aveDir,
-                aveVelo, pairwiseVeloPercent, startX, stopX, startY, stopY)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                """
-                vals = (
-                    features.get("userID"),
-                    features.get("strokeDuration"),
-                    features.get("midStrokeArea"),
-                    features.get("midStrokePressure"),
-                    features.get("directionEndToEnd"),
-                    features.get("averageDirection"),
-                    features.get("averageVelocity"),
-                    features.get("pairwiseVelocityPercentile"),
-                    features.get("startX"),
-                    features.get("stopX"),
-                    features.get("startY"),
-                    features.get("stopY")
-                )
+                    # ---------- Insert into MySQL ----------
+                    sql = """
+                    INSERT INTO swipefeatures
+                    (userID, strokeDuration, midStrokeArea, midStrokePress, dirEndToEnd, aveDir,
+                    aveVelo, pairwiseVeloPercent, startX, stopX, startY, stopY, touchArea, maxVelo, 
+                    minVelo, accel, decel, trajLength, curvature, veloVariance, angleChangeRate, maxPress,
+                     minPress, initPress, pressChangeRate, pressVariance) 
+                    
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s,
+                     %s, %s, %s, %s, %s, %s, %s, %s,
+                     %s, %s, %s, %s, %s, %s, %s, %s, 
+                     %s, %s)
+                    """
 
-                mycursor.execute(sql, vals)
-                mydb.commit()
-                print("Inserted into MySQL successfully.")
+                    # Below is rest of INSERT command when everything is implemented
 
-                # Send acknowledgment to client
-                response = json.dumps({"status": "received"})
-                conn.sendall(response.encode('utf-8'))
+                    # maxIdleTime, straightnessRatio, aveTouchArea, xDisplacement, yDisplacement
 
-            except json.JSONDecodeError as e:
-                print("JSON decode error:", e)
-                print("Raw data:", text)
-            except mysql.connector.Error as e:
-                print("MySQL error:", e)
+                    # Make sure to add %s to the VALUES as well
 
-        mydb.close()
-    conn.close()
-    server.close()
-    # sendToAndroid(token)
-    print(token)
+
+
+                    vals = (
+                        features.get("userID"),
+                        features.get("strokeDuration"),
+                        features.get("midStrokeArea"),
+                        features.get("midStrokePressure"),
+                        features.get("directionEndToEnd"),
+                        features.get("averageDirection"),
+                        features.get("averageVelocity"),
+                        features.get("pairwiseVelocityPercentile"),
+                        features.get("startX"),
+                        features.get("stopX"),
+                        features.get("startY"),
+                        features.get("stopY"),
+                        features.get("touchArea"),
+                        features.get("maxVelo"),
+                        features.get("minVelo"),
+                        features.get("aveAccel"),
+                        features.get("aveDecel"),
+                        features.get("trajLength"),
+                        features.get("curvature"),
+                        features.get("veloVariance"),
+                        features.get("angleChangeRate"),
+                        features.get("maxPress"),
+                        features.get("minPress"),
+                        features.get("initPress"),
+                        features.get("pressChangeRate"),
+                        features.get("pressVariance") #,
+
+                        #For later use once implemented
+
+                        # features.get("maxIdleTime"),
+                        # features.get("straightnessRatio"),
+                        # features.get("aveTouchArea"),
+                        # features.get("xDisplacement"),
+                        # features.get("yDisplacement")
+
+                    )
+
+                    mycursor.execute(sql, vals)
+                    mydb.commit()
+                    print("Inserted into MySQL successfully.")
+
+                    # Send acknowledgment to client
+                    response = json.dumps({"status": "received"})
+                    conn.sendall(response.encode('utf-8'))
+
+                except json.JSONDecodeError as e:
+                    print("JSON decode error:", e)
+                    print("Raw data:", text)
+                except mysql.connector.Error as e:
+                    print("MySQL error:", e)
+
+            mydb.close()
+        conn.close()
+        server.close()
+        # sendToAndroid(token)
